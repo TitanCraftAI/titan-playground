3e5468a79c177d92d84485c8e560769e333561f5
TitanCraftAI <titancraftai@gmail.com>
2025-08-30 22:47:14 -0600

    Create tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        titan: {
+          100: "#E6F0FF",
+          300: "#9BC0FF",
+          500: "#4D8DFF",
+          700: "#1E5CE6",
+          900: "#0F2E73",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
